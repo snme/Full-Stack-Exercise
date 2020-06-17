4bfb5797a6134b8c78347fe4f567834bebe73d1d
Sathya Edamadaka <sathya.edamadaka@gmail.com>
2020-06-17 01:08:43 -0400

    Up to displaying changing data

--- awsgraphql-react/src/index.js
+++ awsgraphql-react/src/index.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
-import gql from 'graphql-tag';
+//import gql from 'graphql-tag';
 import * as serviceWorker from './serviceWorker';
 import AWSAppSyncClient, { AUTH_TYPE } from 'aws-appsync';
 import aws_config from './aws-exports';
+import { ApolloProvider } from 'react-apollo';
 
 /*ReactDOM.render(
   <React.StrictMode>
@@ -14,7 +14,7 @@ import aws_config from './aws-exports';
   document.getElementById('root')
 );*/
 
-import { listEmployees } from './graphql/queries.js';
+//import { listEmployees } from './graphql/queries.js';
 
 const client = new AWSAppSyncClient({
     url: aws_config.aws_appsync_graphqlEndpoint,
@@ -25,13 +25,15 @@ const client = new AWSAppSyncClient({
     }
 });
 
-client.query({
-    query: gql(listEmployees)
-}).then(({ data }) => {
-    console.log(data);
-});
+// client.query({
+//     query: gql(listEmployees)
+// }).then(({ data }) => {
+//     console.log(data);
+// });
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<ApolloProvider client={client}>
+    <App />
+</ApolloProvider>, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
